c4d542881521c81f65ecba320e076a252f78a377
Usuario <usuario@Alejandro-Revilla-2.local>
2026-01-21 18:39:13 -0500

    actualizado para promedio

---
+++ Practica/promedio.py
@@ -0,0 +1,17 @@
+#ingresar 10 numeros, con los 5 primeros calcular el promedio y con los cinco restantes, la mediana.
+numeros_ingreso = input("ingrese 10 numero separados por coma, ejemplo (1,2,3): ")
+numeros = numeros_ingreso.split(",")
+
+#ingresar al leng para dividir la lista en dos y que cada una tenga 5 numero
+longitud = len(numeros)
+division = int(longitud/2)
+
+
+#calcular el promerio
+suma = 0
+for i in range(division):
+    numeros[i] = float(numeros[i])
+    suma = suma + numeros[i]
+
+promedio = suma / division
+print("el promero es:",promedio)
