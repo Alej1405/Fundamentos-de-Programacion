525afcc563af0a741cd5573eb8cc97100927eab5
Usuario <usuario@Alejandro-Revilla-2.local>
2026-01-21 20:29:15 -0500

    actualizado matriz

--- Practica/matriz.py
+++ Practica/matriz.py
@@ -1,10 +1,18 @@
 matriz = [['a','b','c'],['c','d','e'],['f','g','h']]
 
     
-print(matriz)
-for fila in matriz:
-    for elemento in fila:
-        print(elemento)
+#print(matriz)
+# for fila in matriz:
+#     for elemento in fila:
+#         print(elemento)
 
 
-print(matriz[1][1])
+#print(matriz[1][1])
+longitud_m = len(matriz)
+longitud_m_c = len(matriz[1])
+
+for i in range(longitud_m):
+    #print(matriz[i])
+    for i in range(longitud_m_c):
+        print(matriz[i][i])
+
