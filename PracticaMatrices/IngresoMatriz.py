d67dc0fcf5d78f7aa9e119a31cb4f23540a2a5a3
Usuario <usuario@Alejandro-Revilla-2.local>
2026-01-28 19:31:24 -0500

    Actualizando matrices

--- PracticaMatrices/IngresoMatriz.py
+++ PracticaMatrices/IngresoMatriz.py
@@ -1,15 +1,20 @@
 # Programa principal
+'''
+Docstring para PracticaMatrices.IngresoMatriz
+'''
 filas = int(input("Número de filas: "))
 columnas = int(input("Número de columnas: "))
 
+'''
+ingresamos la matriz por teclado
+'''
 matriz = []
 for i in range(filas):
     fila = []
     for j in range(columnas):
-        valor = int(input(f"Ingrese el valor para posición [{i},{j}]: "))
+        valor = float(input(f"Ingrese el valor para posición [{i},{j}]: "))
         fila.append(valor)
     matriz.append(fila)
 
 for i in matriz:
     print(i)
-
